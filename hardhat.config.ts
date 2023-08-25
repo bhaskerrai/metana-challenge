11dca7008e63442e2c2e1e67528d80600d70a59e
Bhasker Rai <88928728+bhaskerrai@users.noreply.github.com>
2023-08-25 14:47:13 +0500

    added sepolia chain id

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,29 @@
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   paths: { tests: "tests" },
+  networks: {
+    hardhat: {
+      chainId: 1337, // Sepolia chainId
+    },
+    sepolia: {
+      url: "https://sepolia.ledgerium.net/rpc", // Sepolia RPC URL
+      chainId: 1729, // Sepolia chainId
+      accounts: {
+        mnemonic: "your-mnemonic-here", // Add your mnemonic for the Sepolia account
+      },
+    },
+    // etherscan: {
+    //   apiKey: {
+    //     sepolia: process.env.ETHERSCAN_API_KEY || "",
+    //   },
+    // },
+  },
 };
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
